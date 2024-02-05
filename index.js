66a116f1575886906e432c0300f8e474387ba8c9
damienlopvet <damienlopvet@gmail.com>
2024-02-05 13:12:17 +0100

    went to mistral api

--- index.js
+++ index.js
@@ -1,43 +1,61 @@
-const { Configuration, OpenAIApi } = require("openai");
-const express = require('express');
-const bodyParser = require('body-parser');
+import MistralClient from "@mistralai/mistralai";
+import express from "express";
+import bodyParser from "body-parser";
+import "dotenv/config";
 const app = express();
-const port= 80
+const port = 80;
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
+
+const apiKey = process.env.MISTRAL_API_KEY || "your_api_key";
+
+const client = new MistralClient(apiKey);
+
+
+app.get("/", async (req, res) => {
+  console.log('api key',process.env.MISTRAL_API_KEY);
+  const chatResponse = await client
+		.chat({
+			model: "mistral-tiny",
+			temperature: 0.1,
+			messages: [{ role: "user", content: "is the chat up and running ?" }],
+		})
+		.then((response) => {
+			res.status(200).json({ message: response.choices[0].message.content });
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(400).json(error.name);
+		});
+  
 });
 
 
-const configuration = new Configuration({
-  organization: process.env.OPENAI_ORG,
-  apiKey: process.env.OPENAI_API,
-});
-const openai = new OpenAIApi(configuration);
+app.post("/api/message", async (req, res) => {
+	console.log(req.body);
+	const messageList = [];
 
-app.get("/", (req, res) => {
-  res.status(200).json({message:'working'})
-});
-
-app.post('/api/message',  async (req, res)=>{
-  const messageList = [];
-
-  const messages = req.body.messages;
+	const messages = req.body.messages;
   const data = messageList.concat(messages);
-  openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [...data],
-  }).then((response) => {
-    
-    res.status(200).json({message: response.data.choices[0].message.content})
-  }).catch((error) => {
-    res.status(400).json({message:error});
-  })
-  
-  
- })
+  console.log('data',data);
+	client
+		.chat({
+			model: "mistral-tiny",
+			messages: [...data],
+		})
+		.then((response) => {
+			res.status(200).json({ message: response.choices[0].message.content });
+		})
+		.catch((error) => {
+			console.log("------------------------error------------------------", error);
+			res.status(400).json({ message: error });
+  	});
+  // res.status(200).json({ message: 'hello' });
+});
 
 app.listen(port);
