ef26ca2d26e45b7815f8af23fec2adf5ef42b9d5
damienlopvet <damienlopvet@gmail.com>
2024-02-07 09:45:50 +0100

    update bash script for deploying in prod

--- prod.sh
+++ prod.sh
@@ -5,6 +5,10 @@ set -e
 
 docker-compose stop
 
+echo "----------------------------Pulling latest changes from git---------------------------"
+
 git pull
+
+echo "----------------------------Building and starting the app---------------------------"
 
 docker-compose -f compose.prod.yaml up
